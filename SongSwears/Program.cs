efa03a483963ccc89122c53c55dd69e7b2ebdd75
kacper.konieczny <kacper.konieczny@microsoft.wsei.edu.pl>
2019-05-22 14:53:32 +0200

    FIRST VERSION

---
+++ SongSwears/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace SongSwears
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+            Console.ReadLine();
+        }
+    }
+
+    class SongAnalysis
+    {
+        public SongAnalysis(string band, string song)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/" + band + "/" + song;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
+        }
+    }
+}
+   
